1dce0f09e74f0c1544a9d6eef9be5caca782be00
Ian Coelho <ian.coel@hotmail.com>
2021-11-08 19:03:00 -0300

    ArraysAndObjects

---
+++ 03-ArraysAndObjects/sandbox.js
@@ -0,0 +1,24 @@
+// arrays
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(10)
+var numbers = [20, 15, 10, 5];
+numbers.push(0);
+// number.push("-5");
+var mixed = ['ryu', 4, 'ken', true];
+mixed.push('test');
+mixed.push(2);
+mixed[0] = 8;
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+//ninja.age = '30';
+//likewise arrays, if a property was declared as a predetermined type, it won't compile if it get assigned to a diferent one
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 15
+};
