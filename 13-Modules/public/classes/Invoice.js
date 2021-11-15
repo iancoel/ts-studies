f266ec1356760bbfaea14c2679223cb3b8cf6303
Ian Coelho <ian.coel@hotmail.com>
2021-11-15 20:09:51 -0300

    modules

---
+++ 13-Modules/public/classes/Invoice.js
@@ -0,0 +1,12 @@
+// classes
+export class Invoice {
+    //This way only works if we use readonly, private or public modifiers
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
---
+++ 13-Modules/public/sandbox.js
@@ -0,0 +1,21 @@
+import { Invoice } from './classes/Invoice.js';
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+//Can't do this because now the modifier is readonly -> invOne.client = 'yoshi';
+invTwo.amount = 400;
+console.log(invoices);
+// const form = document.querySelector('form')!;
+const form = document.querySelector('.new-item-form');
+//console.log(form.children);
+//inputs
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
